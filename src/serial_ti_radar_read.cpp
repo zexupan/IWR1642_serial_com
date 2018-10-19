812ab1a124ba8d53febe9ef125686aaa8b6220f6
jujuejundao <xu325504@hotmail.com>
2018-10-19 09:29:24 -0700

    1

--- src/serial_ti_radar_read.cpp
+++ src/serial_ti_radar_read.cpp
@@ -6,8 +6,18 @@
 //define 5 different messages and the buffer
 uint8_t frame_header[52];
 uint8_t frame_header_sync[8];
-uint8_t buffer[14];
+uint8_t tlv_header[8];
 
+#define frame_header_package_length         *(uint32_t *)(frame_header + 20)
+#define frame_header_frame_number           *(uint32_t *)(frame_header + 24)
+#define frame_header_no_tlv                 *(uint16_t *)(frame_header + 48)
+
+#define tlv_header_type                     *(uint32_t *)(tlv_header)
+#define tlv_header_length                   *(uint32_t *)(tlv_header + 4)
+
+#define tlv_header_type_pointCloud          0x06000000
+#define tlv_header_type_targetObjectList    0x07000000
+#define tlv_header_type_targetIndex         0x08000000
 
 using namespace std;
 
@@ -61,6 +71,10 @@ int main(int argc, char *argv[])
 
 	int got_frame_header = 0;
 	int frame_header_bytes = 52;
+	uint32_t target_frame_number = 0x00000000;
+	uint32_t dataLength;
+	int no_target;
+	int no_input_points;
 
 	while(ros::ok())
 	{
@@ -85,18 +99,58 @@ int main(int argc, char *argv[])
 					break;
 				}
 			}
+
 			
-			//check sum
+			//check sum..................................................................................
+
+
 			if (got_frame_header == 1)
 			{
 				//check for new sync
-				got_frame_header = 0;
+				if (frame_header_frame_number > target_frame_number)
+				{
+					target_frame_number = frame_header_frame_number;
+					got_frame_header = 0;
+				}
+				else
+				{
+					lostsync = 1; //old frame
+					got_frame_header = 0;
+				}
 			}
-			else
-				lostsync = 1; //old frmae
 
 			if (lostsync)
 				break;
+
+			//we got a valid header
+			dataLength = frame_header_package_length - 52;
+			target_frame_number = frame_header_frame_number;
+
+			no_input_points = 0;
+			no_target = 0;
+
+			for (int i = 1; i <= frame_header_frame_number; i++)
+			{
+				for (int i = 1; i <= 8; i++)
+				{
+					uint8_t temp_byte;
+					fd.read(&temp_byte, 1);
+					tlv_header[i] = temp_byte;
+				}
+				if (tlv_header_type == tlv_header_type_pointCloud)
+				{
+					/* code *///...................................................................................
+				}
+				else if (tlv_header_type == tlv_header_type_targetObjectList)
+				{
+					/* code */
+				}
+				else if (tlv_header_type == tlv_header_type_targetIndex)
+				{
+					/* code */
+				}
+			}
+
 
 
 		}

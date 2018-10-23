2f0710909d9f699a917e36e545703f992cd24bd4
jujuejundao <xu325504@hotmail.com>
2018-10-23 07:15:03 -0700

    1

--- src/serial_ti_radar_read.cpp
+++ src/serial_ti_radar_read.cpp
@@ -5,7 +5,6 @@
 
 //define 5 different messages and the buffer
 uint8_t frame_header[52];
-uint8_t frame_header_sync[8];
 uint8_t tlv_header[8];
 
 #define frame_header_package_length         *(uint32_t *)(frame_header + 20)
@@ -15,9 +14,9 @@ uint8_t tlv_header[8];
 #define tlv_header_type                     *(uint32_t *)(tlv_header)
 #define tlv_header_length                   *(uint32_t *)(tlv_header + 4)
 
-#define tlv_header_type_pointCloud          0x06000000
-#define tlv_header_type_targetObjectList    0x07000000
-#define tlv_header_type_targetIndex         0x08000000
+#define tlv_header_type_pointCloud          0x00000006
+#define tlv_header_type_targetObjectList    0x00000007
+#define tlv_header_type_targetIndex         0x00000008
 
 using namespace std;
 
@@ -54,19 +53,19 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	ros::Rate rate(60);
+	ros::Rate rate(100);
 
-	int lostsync = 0;
+	int lostsync = 1;
 	uint8_t testsync[8];
 
-	testsync[1] = 0x02;
-	testsync[2] = 0x01;
-	testsync[3] = 0x04;
-	testsync[4] = 0x03;
-	testsync[5] = 0x06;
-	testsync[6] = 0x05;
-	testsync[7] = 0x08;
-	testsync[8] = 0x07;
+	testsync[0] = 0x02;
+	testsync[1] = 0x01;
+	testsync[2] = 0x04;
+	testsync[3] = 0x03;
+	testsync[4] = 0x06;
+	testsync[5] = 0x05;
+	testsync[6] = 0x08;
+	testsync[7] = 0x07;
 	
 
 	int got_frame_header = 0;
@@ -82,31 +81,42 @@ int main(int argc, char *argv[])
 		{
 			if (got_frame_header == 0)
 			{
-				for (int i = 1; i <= 52; i++)
+				for (int i = 0; i < 52; i++)
 				{
 					uint8_t temp_byte;
 					fd.read(&temp_byte, 1);
 					frame_header[i] = temp_byte;
+//					printf("%02x ", frame_header[i]);
 				}
+//				printf("\n\n");
 			}
 
 			//check sync pattern
-			for (int i = 0; i < 8; ++i)
+			for (int i = 0; i < 8; i++)
 			{
-				if(frame_header[i] != frame_header_sync[i])
+				if(frame_header[i] != testsync[i])
 				{
 					lostsync = 1;
 					break;
 				}
 			}
 
-			
+
 			//check sum..................................................................................
 
 
 			if (got_frame_header == 1)
 			{
 				//check for new sync
+				for (int i = 0; i < 8; i++)
+				{
+					if (frame_header[i] != testsync[i])
+					{
+						lostsync = 1;
+						got_frame_header = 0;
+					}
+				}
+
 				if (frame_header_frame_number > target_frame_number)
 				{
 					target_frame_number = frame_header_frame_number;
@@ -119,8 +129,11 @@ int main(int argc, char *argv[])
 				}
 			}
 
-			if (lostsync)
+			if (lostsync){
+
 				break;
+			}
+
 
 			//we got a valid header
 			dataLength = frame_header_package_length - 52;
@@ -129,7 +142,15 @@ int main(int argc, char *argv[])
 			no_input_points = 0;
 			no_target = 0;
 
-			for (int i = 1; i <= frame_header_frame_number; i++)
+//			for (int i = 0; i < 52; i++)
+//			{
+//				printf("%02x ", frame_header[i]);
+//			}
+
+
+//			cout<<"datalength = "<<dataLength<<endl;
+
+/*			for (int i = 1; i <= frame_header_frame_number; i++)
 			{
 				for (int i = 1; i <= 8; i++)
 				{
@@ -139,41 +160,53 @@ int main(int argc, char *argv[])
 				}
 				if (tlv_header_type == tlv_header_type_pointCloud)
 				{
-					/* code *///...................................................................................
+					//...................................................................................
 				}
 				else if (tlv_header_type == tlv_header_type_targetObjectList)
 				{
-					/* code */
+					//........................................
 				}
 				else if (tlv_header_type == tlv_header_type_targetIndex)
 				{
-					/* code */
+					//.....................................................
 				}
 			}
+*/
+			for (int i = 0; i < dataLength; i++)
+			{
+				uint8_t temp_byte;
+				fd.read(&temp_byte, 1);
+			}
+			cout<<"finished reading data at frame "<<target_frame_number<<endl;
+			double secs =ros::Time::now().toSec();
+			printf("%f\n", secs);
 
-
-
+			lostsync = 1;
 		}
 
 		while(lostsync)
 		{
 			//Looking for sync data for the header
-			int n = 1;
-			while(n <= 8)
+			int n = 0;
+			while(n < 8)
 			{
 				uint8_t temp_byte;
 				fd.read(&temp_byte, 1);
 				if(temp_byte == testsync[n])
+				{
+					frame_header[n] = temp_byte;
 					n++;
-				else n = 1;
+				}
+				else n = 0;
 			}
 
 			//Header is found, sync back
-			if (n == 9)
+			if (n == 8)
 			{
+				printf("found frame header, exit lostsync while loop\n");
 				lostsync = 0;
 				//read header, 52-8 bytes
-				for (int i = 9; i <= 52; i++)
+				for (int i = 8; i < 52; i++)
 				{
 					uint8_t temp_byte;
 					fd.read(&temp_byte, 1);

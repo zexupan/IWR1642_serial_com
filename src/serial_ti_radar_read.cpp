c8d2b4a73450f0632b4abfba3281e55b487ecfb1
jujuejundao <xu325504@hotmail.com>
2018-10-24 02:24:19 -0700

    1

--- src/serial_ti_radar_read.cpp
+++ src/serial_ti_radar_read.cpp
@@ -6,6 +6,7 @@
 //define 5 different messages and the buffer
 uint8_t frame_header[52];
 uint8_t tlv_header[8];
+uint8_t tlv_data_targetObjectList[68]; 
 
 #define frame_header_package_length         *(uint32_t *)(frame_header + 20)
 #define frame_header_frame_number           *(uint32_t *)(frame_header + 24)
@@ -13,6 +14,18 @@ uint8_t tlv_header[8];
 
 #define tlv_header_type                     *(uint32_t *)(tlv_header)
 #define tlv_header_length                   *(uint32_t *)(tlv_header + 4)
+
+#define tlv_data_targetObjectList_trackID                 *(uint32_t *)(tlv_data_targetObjectList)
+#define tlv_data_targetObjectList_posX                    *(float *)(tlv_data_targetObjectList + 4)
+#define tlv_data_targetObjectList_posY                    *(float *)(tlv_data_targetObjectList + 8)
+#define tlv_data_targetObjectList_velX                    *(float *)(tlv_data_targetObjectList + 12)
+#define tlv_data_targetObjectList_velY                    *(float *)(tlv_data_targetObjectList + 16)
+#define tlv_data_targetObjectList_accX                    *(float *)(tlv_data_targetObjectList + 20)
+#define tlv_data_targetObjectList_accY                    *(float *)(tlv_data_targetObjectList + 24)
+#define tlv_data_targetObjectList_covarianceMatrix[9]     *(float *)(tlv_data_targetObjectList + 28) 
+#define tlv_data_targetObjectList_gatingFunctionGain      *(float *)(tlv_data_targetObjectList + 64)
+
+
 
 #define tlv_header_type_pointCloud          0x00000006
 #define tlv_header_type_targetObjectList    0x00000007
@@ -71,7 +84,8 @@ int main(int argc, char *argv[])
 	int got_frame_header = 0;
 	int frame_header_bytes = 52;
 	uint32_t target_frame_number = 0x00000000;
-	uint32_t dataLength;
+	uint32_t frame_dataLength;
+	uint32_t tlv_dataLength;
 	int no_target;
 	int no_input_points;
 
@@ -79,7 +93,7 @@ int main(int argc, char *argv[])
 	{
 		while(lostsync == 0)
 		{
-			if (got_frame_header == 0)
+/*			if (got_frame_header == 0)
 			{
 				for (int i = 0; i < 52; i++)
 				{
@@ -104,7 +118,7 @@ int main(int argc, char *argv[])
 
 			//check sum..................................................................................
 
-
+*/
 			if (got_frame_header == 1)
 			{
 				//check for new sync
@@ -136,7 +150,7 @@ int main(int argc, char *argv[])
 
 
 			//we got a valid header
-			dataLength = frame_header_package_length - 52;
+			frame_dataLength = frame_header_package_length - 52;
 			target_frame_number = frame_header_frame_number;
 
 			no_input_points = 0;
@@ -148,39 +162,69 @@ int main(int argc, char *argv[])
 //			}
 
 
-//			cout<<"datalength = "<<dataLength<<endl;
+//			cout<<"frame_dataLength = "<<frame_dataLength<<endl;
 
-/*			for (int i = 1; i <= frame_header_frame_number; i++)
+			for (int i = 0; i < frame_header_no_tlv; i++)
 			{
-				for (int i = 1; i <= 8; i++)
+				for (int i = 0; i < 8; i++)
 				{
 					uint8_t temp_byte;
 					fd.read(&temp_byte, 1);
 					tlv_header[i] = temp_byte;
 				}
+
+				tlv_dataLength = tlv_header_length - 8;
+
 				if (tlv_header_type == tlv_header_type_pointCloud)
 				{
-					//...................................................................................
+					for (int i = 0; i < tlv_dataLength; i++)
+					{
+						uint8_t temp_byte;
+						fd.read(&temp_byte, 1);
+					}
 				}
+
 				else if (tlv_header_type == tlv_header_type_targetObjectList)
 				{
-					//........................................
+					for (int i = 0; i < tlv_dataLength; i++)
+					{
+						uint8_t temp_byte;
+						fd.read(&temp_byte, 1);
+						tlv_data_targetObjectList[i] = temp_byte;
+					}
+					cout<<"frame no.           "<<target_frame_number<<endl<<endl;
+					cout<<"target no.          "<<tlv_data_targetObjectList_trackID<<endl;
+					cout<<"position     X      "<<tlv_data_targetObjectList_posX<<endl;
+					cout<<"position     Y      "<<tlv_data_targetObjectList_posY<<endl;
+					cout<<"velocity     X      "<<tlv_data_targetObjectList_velX<<endl;
+					cout<<"velocity     Y      "<<tlv_data_targetObjectList_velY<<endl;
+					cout<<"acceleration X      "<<tlv_data_targetObjectList_accX<<endl;
+					cout<<"acceleration Y      "<<tlv_data_targetObjectList_accY<<endl;
 				}
+
 				else if (tlv_header_type == tlv_header_type_targetIndex)
 				{
-					//.....................................................
+					for (int i = 0; i < tlv_dataLength; i++)
+					{
+						uint8_t temp_byte;
+						fd.read(&temp_byte, 1);
+					}
+				}
+				else
+				{
+					cout<<"TLV header wrong, lost sync at frame = "<<target_frame_number<<endl;
+					lostsync = 1;
 				}
 			}
-*/
-			for (int i = 0; i < dataLength; i++)
-			{
-				uint8_t temp_byte;
-				fd.read(&temp_byte, 1);
-			}
-			cout<<"finished reading data at frame "<<target_frame_number<<endl;
-			double secs =ros::Time::now().toSec();
-			printf("%f\n", secs);
 
+//			for (int i = 0; i < frame_dataLength; i++)
+//			{
+//				uint8_t temp_byte;
+//				fd.read(&temp_byte, 1);
+//			}
+//			cout<<"finished reading data at frame "<<target_frame_number<<endl;
+//			double secs =ros::Time::now().toSec();
+//			printf("%f\n", secs);
 			lostsync = 1;
 		}
 

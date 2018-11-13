f0d1ef925fcbb232c1f6b59883f509fa4b6a04b6
jujuejundao <xu325504@hotmail.com>
2018-11-12 20:50:10 -0800

    1

--- src/serial_ti_radar_read.cpp
+++ src/serial_ti_radar_read.cpp
@@ -7,7 +7,10 @@
 //define 5 different messages and the buffer
 uint8_t frame_header[52];
 uint8_t tlv_header[8];
-uint8_t tlv_data_targetObjectList[68]; 
+uint8_t tlv_data_targetObjectList[68];
+double object_list[32][6];
+int object_list_index[32];
+double object_list_range[32];
 
 #define frame_header_package_length         *(uint32_t *)(frame_header + 20)
 #define frame_header_frame_number           *(uint32_t *)(frame_header + 24)
@@ -93,6 +96,8 @@ int main(int argc, char *argv[])
 	uint32_t tlv_dataLength;
 	int no_target;
 	int no_input_points;
+	int last_track_ID = 100;
+	
 
 	while(ros::ok())
 	{	
@@ -220,34 +225,94 @@ int main(int argc, char *argv[])
 
 //					printf("%d\n", no_of_objects);
 
+					
+
 					for (int i = 0; i < no_of_objects; i++)
 					{
-						for (int i = 0; i < 68; i++)
+						for (int j = 0; j < 68; j++)
 						{
 							while(fd.available() <= 0);
 							uint8_t temp_byte;
 							fd.read(&temp_byte, 1);
-							tlv_data_targetObjectList[i] = temp_byte;
+							tlv_data_targetObjectList[j] = temp_byte;
 						}
 						
+
+						object_list_index[i] = tlv_data_targetObjectList_trackID;
+						object_list[i][0] = tlv_data_targetObjectList_posX;
+						object_list[i][1] = tlv_data_targetObjectList_posY;
+						object_list[i][2] = tlv_data_targetObjectList_velX;
+						object_list[i][3] = tlv_data_targetObjectList_velY;
+						object_list[i][4] = tlv_data_targetObjectList_accX;
+						object_list[i][5] = tlv_data_targetObjectList_accY;
+						
 //						cout<<"target no.          "<<tlv_data_targetObjectList_trackID<<endl;
-						printf("target no.   %02x \n", tlv_data_targetObjectList_trackID);
-						cout<<"position     X      "<<tlv_data_targetObjectList_posX<<endl;
-						cout<<"position     Y      "<<tlv_data_targetObjectList_posY<<endl;
-						cout<<"velocity     X      "<<tlv_data_targetObjectList_velX<<endl;
-						cout<<"velocity     Y      "<<tlv_data_targetObjectList_velY<<endl;
-						cout<<"acceleration X      "<<tlv_data_targetObjectList_accX<<endl;
-						cout<<"acceleration Y      "<<tlv_data_targetObjectList_accY<<endl<<endl;
+//						printf("target no.   %02x \n", tlv_data_targetObjectList_trackID);
+//						cout<<"position     X      "<<tlv_data_targetObjectList_posX<<endl;
+//						cout<<"position     Y      "<<tlv_data_targetObjectList_posY<<endl;
+//						cout<<"velocity     X      "<<tlv_data_targetObjectList_velX<<endl;
+//						cout<<"velocity     Y      "<<tlv_data_targetObjectList_velY<<endl;
+//						cout<<"acceleration X      "<<tlv_data_targetObjectList_accX<<endl;
+//						cout<<"acceleration Y      "<<tlv_data_targetObjectList_accY<<endl<<endl;
 
-						posMsg.header.stamp = ros::Time::now();
-						posMsg.header.frame_id = '1';//tlv_data_targetObjectList_trackID;
-						posMsg.point.x = tlv_data_targetObjectList_posX;
-						posMsg.point.y = tlv_data_targetObjectList_posY;
-						posMsg.point.z = 0.0;
-
-						pos0Pub.publish(posMsg);
-
+						
 					}
+
+					int found_last_track_ID = 0;
+					int last_track_ID_index;
+
+					for (int i = 0; i < no_of_objects; i++)
+					{
+						if (last_track_ID == object_list_index[i])
+						{
+							found_last_track_ID = 1;
+							last_track_ID_index = i;
+							break;
+						}
+					}
+
+					if (found_last_track_ID == 0)
+					{
+						for (int i = 0; i < no_of_objects; i++)
+						{
+							object_list_range[i] = object_list[i][0] * object_list[i][0] + object_list[i][1]*object_list[i][1];
+						}
+
+						//sorting of object_list_range
+
+						int object_list_index_counter[no_of_objects];
+						for (int i = 0; i < no_of_objects; i++)
+						{
+							object_list_index_counter[i] = i;
+						}
+
+						for(int i = 0; i<no_of_objects-1;i++)
+						{
+							for(int j = 0; j<no_of_objects-i-1; j++)
+							{
+								if(object_list_range[j] > object_list_range[j+1])
+								{
+									double temp_range = object_list_range[j];
+									object_list_range[j] = object_list_range[j+1];
+									object_list_range[j+1] = temp_range;
+									int temp_key = object_list_index_counter[j];
+									object_list_index_counter[j] = object_list_index_counter[j+1];
+									object_list_index_counter[j+1] = temp_key;
+								}
+							}
+						}
+						//set track id to be the nearest target
+						last_track_ID = object_list_index[object_list_index_counter[0]];
+						last_track_ID_index = object_list_index_counter[0];
+					}
+
+					posMsg.header.stamp = ros::Time::now();
+					posMsg.header.frame_id = '1';//tlv_data_targetObjectList_trackID;
+					posMsg.point.x = object_list[last_track_ID_index][0];
+					posMsg.point.y = object_list[last_track_ID_index][1];
+					posMsg.point.z = 0.0;
+
+					pos0Pub.publish(posMsg);
 				}
 
 				else if (tlv_header_type == tlv_header_type_targetIndex)
@@ -271,171 +336,9 @@ int main(int argc, char *argv[])
 
 			lostsync = 1;
 		}
+
+		
+
 		rate.sleep();
 	}
 }
-
-/*
-	ros::Publisher pos1Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1", 1000);
-	ros::Publisher pos2Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info2", 1000);
-	ros::Publisher pos3Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info3", 1000);
-	ros::Publisher pos4Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info4", 1000);
-	ros::Publisher pos5Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info5", 1000);
-	ros::Publisher pos6Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info6", 1000);
-	ros::Publisher pos7Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info7", 1000);
-	ros::Publisher pos8Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info8", 1000);
-	ros::Publisher pos9Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info9", 1000);
-	ros::Publisher posaPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infoa", 1000);
-	ros::Publisher posbPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infob", 1000);
-	ros::Publisher poscPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infoc", 1000);
-	ros::Publisher posdPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infod", 1000);
-	ros::Publisher posePub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infoe", 1000);
-	ros::Publisher posfPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_infof", 1000);
-	ros::Publisher pos10Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info10", 1000);
-	ros::Publisher pos11Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info11", 1000);
-	ros::Publisher pos12Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info12", 1000);
-	ros::Publisher pos13Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info13", 1000);
-	ros::Publisher pos14Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info14", 1000);
-	ros::Publisher pos15Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info15", 1000);
-	ros::Publisher pos16Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info16", 1000);
-	ros::Publisher pos17Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info17", 1000);
-	ros::Publisher pos18Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info18", 1000);
-	ros::Publisher pos19Pub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info19", 1000);
-	ros::Publisher pos1aPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1a", 1000);
-	ros::Publisher pos1bPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1b", 1000);
-	ros::Publisher pos1cPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1c", 1000);
-	ros::Publisher pos1dPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1d", 1000);
-	ros::Publisher pos1ePub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1e", 1000);
-	ros::Publisher pos1fPub = serial_radar_nh.advertise<geometry_msgs::PointStamped>("radar_info1f", 1000);
-
-	if (tlv_data_targetObjectList_trackID == 0x00)
-						{
-							pos0Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x01)
-						{
-							pos1Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x02)
-						{
-							pos2Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x03)
-						{
-							pos3Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x04)
-						{
-							pos4Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x05)
-						{
-							pos5Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x06)
-						{
-							pos6Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x07)
-						{
-							pos7Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x08)
-						{
-							pos8Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x09)
-						{
-							pos9Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0a)
-						{
-							posaPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0b)
-						{
-							posbPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0c)
-						{
-							poscPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0d)
-						{
-							posdPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0e)
-						{
-							posePub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x0f)
-						{
-							posfPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x10)
-						{
-							pos10Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x11)
-						{
-							pos11Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x12)
-						{
-							pos12Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x13)
-						{
-							pos13Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x14)
-						{
-							pos14Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x15)
-						{
-							pos15Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x16)
-						{
-							pos16Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x17)
-						{
-							pos17Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x18)
-						{
-							pos18Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x19)
-						{
-							pos19Pub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1a)
-						{
-							pos1aPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1b)
-						{
-							pos1bPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1c)
-						{
-							pos1cPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1d)
-						{
-							pos1dPub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1e)
-						{
-							pos1ePub.publish(posMsg);
-						}
-						if (tlv_data_targetObjectList_trackID == 0x1f)
-						{
-							pos1fPub.publish(posMsg);
-						}
-
-
-						*/

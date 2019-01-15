6ec2ec4a2ba539085c5494e2902096085fca9f1f
jujuejundao <xu325504@hotmail.com>
2019-01-14 19:07:04 -0800

    1

--- src/serial_ti_radar_read.cpp
+++ src/serial_ti_radar_read.cpp
@@ -342,3 +342,369 @@ int main(int argc, char *argv[])
 		rate.sleep();
 	}
 }
+
+
+double Est_Target_Position_x;
+double Est_Target_Position_y;
+double Est_Target_Velocity_x;
+double Est_Target_Velocity_y;
+float dt_est = 0.05;
+float Mea_Distance_err = 0.1;
+float Mea_Angle_err = 5;
+float Mea_Speed_err = 1.2;
+float Mea_Angle_Speed_err = 5;
+float Pro_Distance_err = 0.1;
+float Pro_Angle_err = 1.2;
+float sum;
+float Four_Four_temp_00[16];
+float Four_Four_temp_11[16];
+
+
+
+float X_now_1[4][1]=
+{
+    {100},
+    {0},
+    {0},
+    {0}
+
+};
+
+float X_now[4][1]=
+{
+    {0},
+    {0},
+    {0},
+    {0}
+};
+
+float X_now_kp[4][1]=
+{
+    {0},
+    {0},
+    {0},
+    {0}
+};
+
+
+float Y_mea[4][1]=
+{
+    {0},
+    {0},
+    {0},
+    {0}
+};
+
+float Matrix_A[4][4]=
+{
+    {1, 0, dt_est, 0},
+    {0, 1, 0, dt_est},
+    {0, 0, 1, 0},
+    {0, 0, 0, 1}
+};
+
+float Matrix_A_T[4][4]=
+{
+    {1, 0, 0, 0},
+    {0, 1, 0, 0},
+    {dt_est, 0, 1, 0},
+    {0, dt_est, 0, 1}
+};
+
+//Measured Covariance Matrix
+float R_now[4][4]=
+{
+    {Mea_Distance_err*Mea_Distance_err, 0, 0, 0},
+    {0, Mea_Angle_err*Mea_Angle_err, 0, 0},
+    {0, 0,Mea_Speed_err*Mea_Speed_err, 0},
+    {0, 0, 0, Mea_Angle_Speed_err*Mea_Angle_Speed_err}
+};
+
+//Predicted Covariance matrix
+float Q_now[4][4]=
+{
+    {Pro_Distance_err*Pro_Distance_err*dt_est*dt_est*dt_est*dt_est/4,0,Pro_Distance_err*Pro_Distance_err*dt_est*dt_est*dt_est/2,0},
+    {0, Pro_Angle_err*Pro_Angle_err*dt_est*dt_est*dt_est*dt_est/4, 0, Pro_Angle_err*Pro_Angle_err*dt_est*dt_est*dt_est/2},
+    {Pro_Distance_err*Pro_Distance_err*dt_est*dt_est/2, 0, Pro_Distance_err*Pro_Distance_err*dt_est, 0},
+    {0, Pro_Angle_err*Pro_Angle_err*dt_est*dt_est/2, 0, Pro_Angle_err*Pro_Angle_err*dt_est}
+};
+
+float P_now_kp[4][4]=
+{
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+float P_now_1[4][4]=
+{
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+float Kalman_Gain[4][4]=
+{
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+float Kalman_Gain_T[4][4]=
+{
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
+
+float Four_Four_temp[4][4]=
+{
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+
+float Four_Four_temp_1[4][4]=
+{
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0}
+};
+
+float Four_One_temp[4][1]=
+{
+    {0},
+    {0},
+    {0},
+    {0}
+};
+
+float Four_Four_I[4][4]=
+{
+    {1,0,0,0},
+    {0,1,0,0},
+    {0,0,1,0},
+    {0,0,0,1}
+};
+
+
+int radar_estimator(double Pos_x, double Pos_y, double Vel_x, double Vel_y)
+{
+
+    //Compute the processed state
+    sum = 0;
+    for(int c=0; c<4;c++){
+         for(int d=0; d<1; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Matrix_A[c][k] * X_now_1[k][d];
+            }
+            X_now_kp[c][d]= sum;
+            sum = 0;
+        }
+
+    }
+
+    //Compute Pkp Covariance Matrix
+    sum = 0;
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Matrix_A[c][k] * P_now_1[k][d];
+            }
+            Four_Four_temp[c][d]= sum;
+            sum = 0;
+        }
+
+    }
+
+    sum = 0;
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Four_Four_temp[c][k] * Matrix_A_T[k][d];
+            }
+            P_now_kp[c][d]= sum;
+            sum = 0;
+        }
+    }
+
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            P_now_kp[c][d] = P_now_kp[c][d] + Q_now[c][d];
+        }
+    }
+
+
+    //Compute the Kalman Gain
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            Four_Four_temp[c][d] = R_now[c][d] + P_now_kp[c][d];
+        }
+    }
+
+    Four_Four_temp_00[0] = Four_Four_temp[0][0];
+    Four_Four_temp_00[1] = Four_Four_temp[0][1];
+    Four_Four_temp_00[2] = Four_Four_temp[0][2];
+    Four_Four_temp_00[3] = Four_Four_temp[0][3];
+    Four_Four_temp_00[4] = Four_Four_temp[1][0];
+    Four_Four_temp_00[5] = Four_Four_temp[1][1];
+    Four_Four_temp_00[6] = Four_Four_temp[1][2];
+    Four_Four_temp_00[7] = Four_Four_temp[1][3];
+    Four_Four_temp_00[8] = Four_Four_temp[2][02];
+    Four_Four_temp_00[9] = Four_Four_temp[2][1];
+    Four_Four_temp_00[10] = Four_Four_temp[2][2];
+    Four_Four_temp_00[11] = Four_Four_temp[2][3];
+    Four_Four_temp_00[12] = Four_Four_temp[3][0];
+    Four_Four_temp_00[13] = Four_Four_temp[3][1];
+    Four_Four_temp_00[14] = Four_Four_temp[3][2];
+    Four_Four_temp_00[15] = Four_Four_temp[3][3];
+
+    if(inverse4x4(Four_Four_temp_00, Four_Four_temp_11) == false)
+        {
+            for(int i=0; i<4; i++)
+                X_now[i][0] = X_now_kp[i][0];
+            printf("matrix inverse failed\n");
+            return false;
+        }
+
+    Four_Four_temp[0][0] = Four_Four_temp_11[0];
+    Four_Four_temp[0][1] = Four_Four_temp_11[1];
+    Four_Four_temp[0][2] = Four_Four_temp_11[2];
+    Four_Four_temp[0][3] = Four_Four_temp_11[3];
+    Four_Four_temp[1][0] = Four_Four_temp_11[4];
+    Four_Four_temp[1][1] = Four_Four_temp_11[5];
+    Four_Four_temp[1][2] = Four_Four_temp_11[6];
+    Four_Four_temp[1][3] = Four_Four_temp_11[7];
+    Four_Four_temp[2][0] = Four_Four_temp_11[8];
+    Four_Four_temp[2][1] = Four_Four_temp_11[9];
+    Four_Four_temp[2][2] = Four_Four_temp_11[10];
+    Four_Four_temp[2][3] = Four_Four_temp_11[11];
+    Four_Four_temp[3][0] = Four_Four_temp_11[12];
+    Four_Four_temp[3][1] = Four_Four_temp_11[13];
+    Four_Four_temp[3][2] = Four_Four_temp_11[14];
+    Four_Four_temp[3][3] = Four_Four_temp_11[15];
+
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + P_now_kp[c][k] * Four_Four_temp[k][d];
+            }
+            Kalman_Gain[c][d]= sum;
+            sum = 0;
+        }
+    }
+
+
+
+    //compute the filtered state
+    Y_mea[0][0] = Pos_x;
+    Y_mea[1][0] = Pos_y;
+    Y_mea[2][0] = Vel_x;
+    Y_mea[2][0] = Vel_y;
+
+    for(int c=0; c<4; c++){
+        for(int d=0; d<1; d++){
+            Four_One_temp[c][d] = Y_mea[c][d] - X_now_kp[c][d];
+        }
+    }
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<1; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Kalman_Gain[c][k] * Four_One_temp[k][d];
+            }
+            X_now[c][d]= sum + X_now_kp[c][d];
+            sum = 0;
+        }
+    }
+
+
+
+    Est_Target_Position_x = X_now[0][0];
+    Est_Target_Position_y = X_now[1][0];
+    Est_Target_Velocity_x = X_now[2][0];
+    Est_Target_Velocity_y = X_now[3][0];
+
+    //update covariance matrix
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            Four_Four_temp[c][d] = Four_Four_I[c][d] -  Kalman_Gain[c][d];
+        }
+    }
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Four_Four_temp[c][k] * P_now_kp[k][d];
+            }
+            P_now_1[c][d]= sum;
+            sum = 0;
+        }
+    }
+
+    for(int c=0; c<4; c++){
+        for(int d=0; d<c; d++){
+            float temp = Four_Four_temp[d][c];
+            Four_Four_temp[d][c] = Four_Four_temp[c][d];
+            Four_Four_temp[c][d] = temp;
+        }
+    }
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + P_now_1[c][k] * Four_Four_temp[k][d];
+            }
+            Four_Four_temp_1[c][d]= sum;
+            sum = 0;
+        }
+    }
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Kalman_Gain[c][k] * R_now[k][d];
+            }
+            Four_Four_temp[c][d]= sum;
+            sum = 0;
+        }
+    }
+
+    for(int c=0; c<4; c++){
+        for(int d=0; d<4; d++){
+            Kalman_Gain_T[c][d] = Kalman_Gain[d][c];
+        }
+    }
+
+    sum = 0;
+    for(int c=0; c<4;c++){
+        for(int d=0; d<4; d++){
+            for(int k=0; k<4; k++){
+                sum= sum + Four_Four_temp[c][k] * Kalman_Gain_T[k][d];
+            }
+            P_now_1[c][d]= sum + Four_Four_temp_1[c][d];
+            sum = 0;
+        }
+    }
+
+    //update the state matrix
+    for(int c=0; c<4; c++){
+        for(int d=0; d<1; d++){
+            X_now_1[c][d] = X_now[c][d];
+        }
+    }
+
+    return 1;
+}
